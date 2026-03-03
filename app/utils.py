393aed05b9a26cdbf550ca7cafad73a834d0f13d
Atharv <172103387+zokomom@users.noreply.github.com>
2026-03-03 23:07:54 +0530

    feat:added utilities for hashing of password and verification

---
+++ app/utils.py
@@ -0,0 +1,10 @@
+from passlib.context import CryptContext
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def hash(password: str):
+    return pwd_context.hash(password)
+
+
+def verify_password(received_pwd, hashed_pwd):
+    return pwd_context.verify(received_pwd, hashed_pwd)
