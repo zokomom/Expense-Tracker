41ae472afa5c1db0226e4ca594e08cdb8a9484ef
Atharv <172103387+zokomom@users.noreply.github.com>
2026-03-05 00:52:44 +0530

    feat:created expense in and out schema for validation

--- app/schemas.py
+++ app/schemas.py
@@ -34,3 +34,20 @@ class TokenData:
 class TokenOut(BaseModel):
     access_token: str
     token_type: str
+
+
+class ExpenseOut(BaseModel):
+    id: int
+    user_id: int
+    amount: float
+    category: str
+
+
+class ExpenseIn(BaseModel):
+    amount: float
+    category: str
+
+    @field_validator("category")
+    @classmethod
+    def normalize_category(cls, value):
+        return value.strip().capitalize()
