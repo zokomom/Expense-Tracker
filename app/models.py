01ba2081584bac9890de190ca3c9612c5d834964
Atharv <172103387+zokomom@users.noreply.github.com>
2026-03-04 17:29:48 +0530

    feat : created Expense model

--- app/models.py
+++ app/models.py
@@ -1,10 +1,24 @@
 from .database import Base
-from sqlalchemy import Column
-from sqlalchemy.types import Integer, String
+from sqlalchemy import Column, ForeignKey
+from sqlalchemy.types import Integer, String, TIMESTAMP, DECIMAL
+from sqlalchemy.sql.expression import text
 
 
 class User(Base):
-    __tablename__ = "Users"
+    __tablename__ = "users"
     user_id = Column(Integer, primary_key=True, nullable=False)
     email = Column(String, unique=True, nullable=False)
     password = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text('now()'))
+
+
+class Expense(Base):
+    __tablename__ = "expenses"
+    id = Column(Integer, primary_key=True, nullable=False)
+    user_id = Column(Integer, ForeignKey(
+        "users.user_id", ondelete="CASCADE"), nullable=False)
+    amount = Column(DECIMAL, nullable=False)
+    category = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text('now()'))
