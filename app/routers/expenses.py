3062b503c95ca0500ab32ca7b12d729cce779f57
Atharv <172103387+zokomom@users.noreply.github.com>
2026-03-05 14:01:39 +0530

    feat: introduced delete_expense endpoint

--- app/routers/expenses.py
+++ app/routers/expenses.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
 from ..schemas import ExpenseOut, ExpenseIn
 from ..database import get_db
@@ -21,8 +21,24 @@ def get_expenses(db: Session = Depends(get_db), get_user: int = Depends(get_acce
 
 @router.post("", response_model=ExpenseOut, status_code=status.HTTP_201_CREATED)
 def create_expense(expense: ExpenseIn, db: Session = Depends(get_db), get_user: int = Depends(get_access_token)):
-    create_expense = Expense(user_id=get_user.user_id,**expense.model_dump())
+    create_expense = Expense(user_id=get_user.user_id, **expense.model_dump())
     db.add(create_expense)
     db.commit()
     db.refresh(create_expense)
     return create_expense
+
+
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_expense(id: int, db: Session = Depends(get_db), get_user: int = Depends(get_access_token)):
+    delete_expense_query = db.query(Expense).filter(
+        id == Expense.id).first()
+
+    if not delete_expense_query:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"User does not exist.")
+
+    if not get_user.user_id == delete_expense_query.user_id:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
+    db.delete(delete_expense_query)
+    db.commit()
